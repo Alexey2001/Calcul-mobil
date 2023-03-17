3ea0092a89da63cd1152cf047fc6def60c01bcf9
Alexey2001 <101454253+Alexey2001@users.noreply.github.com>
2023-03-17 20:53:55 +0200

    Create MainActivity3.java

---
+++ MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.car_rental;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
