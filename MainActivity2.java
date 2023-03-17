cec5486f98547a0dcbd40ef822146d97295cf3b8
Alexey2001 <101454253+Alexey2001@users.noreply.github.com>
2023-03-17 20:53:31 +0200

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,48 @@
+package com.example.car_rental;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button select = (Button) findViewById(R.id.btn);
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+
+    });
+
+        Button select2 = (Button) findViewById(R.id.btn2);
+
+        select2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+
+        });
+
+        Button select3 = (Button) findViewById(R.id.btn2);
+
+        select3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+
+        });
+}}
